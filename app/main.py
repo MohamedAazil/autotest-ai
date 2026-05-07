326d073c4819057cbcf4e46e40bdfbe618e34a0b
MohamedAazil <mohamedaazil1106@gmail.com>
2026-05-08 01:14:14 +0530

    [Feat] BE: Authentication

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-
+from fastapi import FastAPI, Request, Depends
+from .auth import resolve_installation_context
 app = FastAPI()
 
 @app.get("/")
@@ -7,7 +7,7 @@ def root():
     return {"message": "Server is running", "status": 200} 
 
 @app.post("/webhook")
-async def webhook(req: Request):
+async def webhook(req: Request, installation_id = Depends(resolve_installation_context)):
     payload = await req.json()
     print("Webhook received")
     print(payload)
